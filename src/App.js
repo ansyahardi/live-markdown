e35774fd6f956fd57967ef139c86ad182c68b213
ardiansyah <ardiansyah.as97@gmail.com>
2020-09-23 13:15:45 +0700

    membuat live markdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function App() {
+  const [input, setInput] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Live editor untuk membuat markdown</h1>
+      <div className="editor">
+        <textarea name="editor" value={input} onChange={(e) => setInput(e.target.value)} className="live-editor"></textarea>
+      </div>
+      <ReactMarkdown
+        source={input}
+        className="hasil"
+        renderers={{
+          code: Component
+        }}
+      />
     </div>
   );
 }
+
+const Component = ({ value, language }) => {
+  return (
+    <SyntaxHighlighter language={language ?? null} style={docco}>
+      {value ?? ''}
+    </SyntaxHighlighter>
+  );
+};
 
 export default App;
